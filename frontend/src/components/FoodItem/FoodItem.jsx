91c169a68eae3e44bb2735a507ee3a52948ae224
xoumik <shoumikroy2004@gmail.com>
2024-08-09 01:38:41 +0530

    Food-items

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,20 @@
+import { assets } from "../../assets/assets";
+import "./FoodItem.css";
+const FoodItem = ({ id, name, price, description, image }) => {
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img src={image} alt="" className="food-item-image" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
+};
+export default FoodItem;
