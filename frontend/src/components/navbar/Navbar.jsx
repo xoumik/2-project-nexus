d0002b4d328971630fd50049fb96aefbe4ff2f52
xoumik <shoumikroy2004@gmail.com>
2024-08-11 00:23:14 +0530

    fixes

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = ({ setShowLogin }) => {
 
   return (
     <div className="navbar">
-      <h2 className="logo">Delight</h2>
+      <Link to="/">
+        <h2 className="logo">Delight</h2>
+      </Link>
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -42,7 +44,9 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
           <div className="dot"></div>
         </div>
         <button onClick={() => setShowLogin(true)}>Sign In</button>
