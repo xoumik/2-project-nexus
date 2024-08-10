474d1bb11e5dab4f8d6566bfcadac2eaeb378e10
xoumik <shoumikroy2004@gmail.com>
2024-08-10 13:07:02 +0530

    app

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -16,10 +16,6 @@ const StoreContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
-
   const contextValue = {
     food_list,
     cartItems,
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import { useState } from "react";
 import FoodDisplay from "../../components/foodDisplay/FoodDisplay";
+import AppDownload from "../../components/App/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -12,6 +13,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
