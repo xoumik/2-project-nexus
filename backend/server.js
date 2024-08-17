64e70d5d34e82a101181a7ca403ed86d5496bda8
xoumik <shoumikroy2004@gmail.com>
2024-08-18 02:36:40 +0530

    Register User

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 //app config
 const app = express();
@@ -19,6 +20,7 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
